5dc148e770264ac605fb902101c052d9f00791f8
Alkaiir <ivv.ven0@gmail.com>
2024-03-28 13:30:15 +0700

    Добавлено использование моделей

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,19 +2,20 @@
 
 namespace Controller;
 
-use Illuminate\Database\Capsule\Manager as DB;
+use Model\Post;
 use Src\View;
 
 class Site
 {
     public function index(): string
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::all();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
 
-    public function hello(): string
+
+        public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
