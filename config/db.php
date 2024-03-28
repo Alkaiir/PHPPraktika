9c417626709d7b321a8723e7102b880cccb24250
Alkaiir <ivv.ven0@gmail.com>
2024-03-28 12:57:38 +0700

    Работа с illuminate

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'iravtvys_m4',
+    'username' => 'iravtvys',
+    'password' => 'Jidmi7',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
